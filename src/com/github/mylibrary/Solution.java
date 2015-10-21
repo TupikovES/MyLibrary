fcacfa4cb14e0f044880223932a49fc4a9e5463d
TupikovES <evgeniy.tupikov@gmail.com>
2015-10-22 01:39:30 +0400

    Библиотека

--- src/com/github/mylibrary/Solution.java
+++ src/com/github/mylibrary/Solution.java
@@ -1,10 +1,28 @@
 package com.github.mylibrary;
 
+import com.github.mylibrary.module.ArrayObject;
+
 /**
  * Created by x3mib_000 on 21.10.2015.
  */
 public class Solution {
     public static void main(String[] args) {
+        ArrayObject a = new ArrayObject();
+        ArrayObject b = new ArrayObject();
+        ArrayObject c = new ArrayObject();
+        a.addElem("Номер");
+        a.addElem(1);
+
+
+
+        b.addElem(a);
+        b.addElem(a);
+        b.addElem(a);
+
+
+        c = (ArrayObject)b.getArrObject(0);
+
+        System.out.println(c.getArrObject(1));
 
     }
 }
--- src/com/github/mylibrary/module/ArrayObject.java
+++ src/com/github/mylibrary/module/ArrayObject.java
@@ -4,4 +4,49 @@ package com.github.mylibrary.module;
  * Created by x3mib_000 on 21.10.2015.
  */
 public class ArrayObject {
+
+    private Object[] arrObject = new Object[0];
+    private int lengthArray = 0;
+    private int lastIndex = 0;
+
+    private void reSize(int size) {
+        Object[] newArr = new Object[lengthArray + size];
+        if (size > 0) {
+            for (int i = 0; i < lengthArray; i++) {
+                newArr[i] = arrObject[i];
+                lastIndex = i + 1;
+            }
+        } else {
+            for (int i = 0; i < lengthArray + size; i++) {
+                newArr[i] = arrObject[i];
+            }
+        }
+
+        //lastIndex += 1;
+        arrObject = newArr;
+        lengthArray += size;
+    }
+
+    public void addElem(int elem){
+        reSize(1);
+
+        arrObject[lastIndex] = elem;
+    }
+
+    public void addElem(String elem){
+        reSize(1);
+
+        arrObject[lastIndex] = elem;
+    }
+
+    public void addElem(ArrayObject elem){
+        reSize(1);
+
+        arrObject[lastIndex] = elem;
+    }
+
+    public Object getArrObject(int index) {
+        return arrObject[index];
+    }
+
 }
