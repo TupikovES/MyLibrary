779bdffb7d2cec578dc36b70f657cc7b5abb48a9
TupikovES <evgeniy.tupikov@gmail.com>
2015-10-21 21:55:57 +0400

    Библиотека

---
+++ src/com/github/mylibrary/Solution.java
@@ -0,0 +1,10 @@
+package com.github.mylibrary;
+
+/**
+ * Created by x3mib_000 on 21.10.2015.
+ */
+public class Solution {
+    public static void main(String[] args) {
+
+    }
+}
---
+++ src/com/github/mylibrary/module/ArrayObject.java
@@ -0,0 +1,7 @@
+package com.github.mylibrary.module;
+
+/**
+ * Created by x3mib_000 on 21.10.2015.
+ */
+public class ArrayObject {
+}
